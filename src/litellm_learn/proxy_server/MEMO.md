d7665041e1a7abf61fa4028387be8e2e476631f6
fdshg693 <xingwanmeixiong@gmail.com>
2026-01-18 16:43:46 +0900

    ローカルプロキシサーバー経由でのOpenAI APIアクセス例を追加

---
+++ src/litellm_learn/proxy_server/MEMO.md
@@ -0,0 +1,5 @@
+以下で示すコマンドを使用して、詳細なデバッグ情報を有効にしてlitellmプロキシサーバーを起動できます。
+```shell
+litellm --config path/to/config.yaml --detailed_debug
+```
+現状、Connection errorが解消できない（そもそも、4000ポートにアクセスできない）
---
+++ src/litellm_learn/use_proxy.py
@@ -0,0 +1,11 @@
+# Connection errorが解消できない
+
+import openai  # openai v1.0.0+
+
+client = openai.OpenAI(api_key="anything", base_url="http://localhost:4000")
+response = client.chat.completions.create(
+    model="gpt-4o",
+    messages=[{"role": "user", "content": "1+1"}],
+)
+
+print(response.choices[0].message.content)
